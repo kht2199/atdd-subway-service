82514259e8aef683f4c8b95c6e2b572db65e1957
kht2199 <kht2199@gmail.com>
2021-07-16 01:33:08 +0900

    refactor: 정렬된 station 획득 로직 sections로 이동

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -63,39 +63,7 @@ public class Line extends BaseEntity {
             return Collections.emptyList();
         }
 
-        List<Station> stations = new ArrayList<>();
-        Station downStation = findUpStation();
-        stations.add(downStation);
-
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = sections.stream()
-                .filter(it -> it.getUpStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getDownStation();
-            stations.add(downStation);
-        }
-
-        return stations;
-    }
-
-    private Station findUpStation() {
-        Station downStation = sections.get(0).getUpStation();
-        while (downStation != null) {
-            Station finalDownStation = downStation;
-            Optional<Section> nextLineStation = sections.stream()
-                .filter(it -> it.getDownStation() == finalDownStation)
-                .findFirst();
-            if (!nextLineStation.isPresent()) {
-                break;
-            }
-            downStation = nextLineStation.get().getUpStation();
-        }
-
-        return downStation;
+        return sections.orderedStations();
     }
 
     public void addLineStation(Station upStation, Station downStation, int distance) {
--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,19 +1,21 @@
 package nextstep.subway.line.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 
+import nextstep.subway.station.domain.Station;
+
 @Embeddable
 public class Sections {
 
 	@OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
-	private List<Section> sections = new ArrayList<>();
+	private final List<Section> sections = new ArrayList<>();
 
 	public void add(Section section) {
 		this.sections.add(section);
@@ -23,16 +25,8 @@ public class Sections {
 		return this.sections.isEmpty();
 	}
 
-	public List<Section> getSections() {
-		return Collections.unmodifiableList(sections);
-	}
-
 	public Stream<Section> stream() {
 		return sections.stream();
-	}
-
-	public Section get(int i) {
-		return sections.get(i);
 	}
 
 	public int size() {
@@ -41,5 +35,40 @@ public class Sections {
 
 	public void remove(Section it) {
 		this.sections.remove(it);
+	}
+
+	public List<Station> orderedStations() {
+		List<Station> stations = new ArrayList<>();
+		Station downStation = findUpStation();
+		stations.add(downStation);
+
+		while (downStation != null) {
+			Station finalDownStation = downStation;
+			Optional<Section> nextLineStation = sections.stream()
+				.filter(it -> it.getUpStation() == finalDownStation)
+				.findFirst();
+			if (!nextLineStation.isPresent()) {
+				break;
+			}
+			downStation = nextLineStation.get().getDownStation();
+			stations.add(downStation);
+		}
+		return stations;
+	}
+
+	private Station findUpStation() {
+		Station downStation = sections.get(0).getUpStation();
+		while (downStation != null) {
+			Station finalDownStation = downStation;
+			Optional<Section> nextLineStation = sections.stream()
+				.filter(it -> it.getDownStation() == finalDownStation)
+				.findFirst();
+			if (!nextLineStation.isPresent()) {
+				break;
+			}
+			downStation = nextLineStation.get().getUpStation();
+		}
+
+		return downStation;
 	}
 }
